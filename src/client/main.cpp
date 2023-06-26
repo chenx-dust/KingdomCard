2bb96f4173c3f6b3a6a8d411d7366eefbbc249eb
Chen Xijun <chenxijun2247338@hotmail.com>
2023-06-26 09:22:03 +0800

    init: Initial Commit

---
+++ src/client/main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include <QPushButton>
+#include "ui/MainWindow.h"
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return QApplication::exec();
+}
---
+++ src/client/ui/MainWindow.cpp
@@ -0,0 +1,12 @@
+#include "ui/MainWindow.h"
+#include "ui/ui_MainWindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent) :
+        QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
